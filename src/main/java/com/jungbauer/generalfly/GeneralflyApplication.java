7982e79167ef09f86f08b75235f05bb26628082d
Johann Jungbauer <jungbauer@gmail.com>
2025-01-20 11:55:31 +0200

    feat: initial commit

---
+++ src/main/java/com/jungbauer/generalfly/GeneralflyApplication.java
@@ -0,0 +1,13 @@
+package com.jungbauer.generalfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralflyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jungbauer/generalfly/GeneralflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungbauer.generalfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
