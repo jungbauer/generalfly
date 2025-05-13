f05c3f430b58c56aa33cf861f094cfb4768a2646
Johann Jungbauer <jungbauer@gmail.com>
2025-05-13 16:28:40 +0200

    feat(user): adds custom validation matching pwd at registration

---
+++ src/main/java/com/jungbauer/generalfly/validation/PasswordsMatch.java
@@ -0,0 +1,24 @@
+package com.jungbauer.generalfly.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ TYPE, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = PasswordsMatchValidator.class)
+@Documented
+public @interface PasswordsMatch {
+    String message() default "Passwords don't match";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
