af84bd928463eaddf737a84a325b9d13e817ac7c
Johann Jungbauer <jungbauer@gmail.com>
2025-10-26 13:13:08 +0200

    build: update error controller for new spring boot

--- src/main/java/com/jungbauer/generalfly/controller/CustomErrorController.java
+++ src/main/java/com/jungbauer/generalfly/controller/CustomErrorController.java
@@ -2,7 +2,7 @@ package com.jungbauer.generalfly.controller;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.boot.webmvc.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
