c90cac5ead920f017121687b6fd4962a0d8fc2aa
Johann Jungbauer <jungbauer@gmail.com>
2026-03-14 13:01:45 +0200

    feat: adds request ip to logging message

--- src/main/java/com/jungbauer/generalfly/controller/nhl/NhlController.java
+++ src/main/java/com/jungbauer/generalfly/controller/nhl/NhlController.java
@@ -3,7 +3,10 @@ package com.jungbauer.generalfly.controller.nhl;
 import com.jungbauer.generalfly.dto.nhl.api.ClubSeasonSchedule;
 import com.jungbauer.generalfly.dto.nhl.api.GameCenterPlayByPlay;
 import com.jungbauer.generalfly.dto.nhl.api.Standings;
+import com.jungbauer.generalfly.service.DumpLogService;
 import com.jungbauer.generalfly.service.nhl.NhlApiService;
+import com.jungbauer.generalfly.service.nhl.NhlDataService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,29 +20,44 @@ import java.util.concurrent.TimeUnit;
 @RequestMapping("/nhl")
 public class NhlController {
     private final NhlApiService nhlApiService;
+    private final DumpLogService dumpLogService;
 
-    public NhlController(NhlApiService nhlApiService) {
+    public NhlController(NhlApiService nhlApiService, DumpLogService dumpLogService) {
         this.nhlApiService = nhlApiService;
+        this.dumpLogService = dumpLogService;
     }
 
     @GetMapping("/standings")
-    public ResponseEntity<Standings> standings() {
+    public ResponseEntity<Standings> standings(HttpServletRequest request) {
+        dumpLogService.logMessage("NhlController", "standings", "Standings called. ip: " + getIpAddress(request));
         return ResponseEntity.ok()
                 .cacheControl(CacheControl.maxAge(12, TimeUnit.HOURS))
                 .body(nhlApiService.getStandingsNow());
     }
 
     @GetMapping("/club-schedule")
-    public ResponseEntity<ClubSeasonSchedule> clubSchedule(@RequestParam(name = "team") String teamCode, @RequestParam(name = "season") String season) {
+    public ResponseEntity<ClubSeasonSchedule> clubSchedule(HttpServletRequest request, @RequestParam(name = "team") String teamCode, @RequestParam(name = "season") String season) {
+        String msg = "club schedule called: " + teamCode + " : " + season + ", ip: " + getIpAddress(request);
+        dumpLogService.logMessage("NhlController", "clubSchedule", msg);
         return ResponseEntity.ok()
                 .cacheControl(CacheControl.maxAge(12, TimeUnit.HOURS))
                 .body(nhlApiService.getClubSeasonSchedule(teamCode, season));
     }
 
     @GetMapping("/gamecenter-playbyplay")
-    public ResponseEntity<GameCenterPlayByPlay> gamecenterPlayByPlay(@RequestParam(name = "gameId") String gameId) {
+    public ResponseEntity<GameCenterPlayByPlay> gamecenterPlayByPlay(HttpServletRequest request, @RequestParam(name = "gameId") String gameId) {
+        String msg = "pbp for game: " + gameId + ", ip: " + getIpAddress(request);
+        dumpLogService.logMessage("NhlController", "gamecenterPlayByPlay", msg);
         return ResponseEntity.ok()
                 .cacheControl(CacheControl.maxAge(12, TimeUnit.HOURS))
                 .body(nhlApiService.getGameCenterPlayByPlay(gameId));
+    }
+
+    private String getIpAddress(HttpServletRequest request) {
+        String ipAddress = request.getHeader("X-Forwarded-For");
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+        return ipAddress;
     }
 }
